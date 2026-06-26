af24c7885452f2da2ee7bb4c99784bf502282cc8
kowellaloo9698-afk <kowellaloo9698@gmail.com>
2026-06-26 02:50:54 +0000

    Day 1 m2: tokenize line with strtok, strip newline, NULL-terminate argv, echo tokens

--- shell.c
+++ shell.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 int main(){
 	char *line=NULL;
 	size_t len=0;
 	while(1){
 		printf("> ");
-		if(getline(&line,&len,stdin)==-1)break;
-		printf("You typed: %s",line);
+		ssize_t n=getline(&line,&len,stdin);
+		if(n==-1)break;
+		if(n>0 && line[n-1]=='\n')line[n-1]='\0';
+		char *tokens[64];
+		int count=0;
+		char *tok=strtok(line," ");
+		while(tok!=NULL){
+			tokens[count]=tok;
+			count++;
+			tok=strtok(NULL," ");
+		}
+		for(int i=0;i<count;i++)printf("token[%d] = %s\n",i,tokens[i]);
 	}
 	free(line);
 	return 0;
 }
-
