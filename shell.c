5b2aaefab537bed7b67fd9054f81f56d6302ba38
kowellaloo9698-afk <kowellaloo9698@gmail.com>
2026-06-26 07:08:38 +0000

    Day 2: fork + execvp + wait to run external commands

--- shell.c
+++ shell.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 int main(){
 	char *line=NULL;
 	size_t len=0;
 	while(1){
 		printf("> ");
 		ssize_t n=getline(&line,&len,stdin);
-		if(n==-1)break;
+		if(n==-1){
+			printf("\n");
+			break;
+			}
 		if(n>0 && line[n-1]=='\n')line[n-1]='\0';
 		char *tokens[64];
 		int count=0;
@@ -17,7 +23,17 @@ int main(){
 			count++;
 			tok=strtok(NULL," ");
 		}
-		for(int i=0;i<count;i++)printf("token[%d] = %s\n",i,tokens[i]);
+		tokens[count]=NULL;
+		pid_t rc=fork();
+		if(rc<0)perror("fork");
+		else if(rc==0){
+			execvp(tokens[0],tokens);
+			perror("execvp");
+			exit(1);
+			}
+		else{
+		        wait(NULL);
+		}
 	}
 	free(line);
 	return 0;
